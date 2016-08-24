05490a8b60917b43ce73f98540da8065b7872b8f
qiuwenxi <qiuwenxi@zbj.com>
2016-08-24 11:46:22 +0800

    Add wphp56

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cp ~/.ssh/id_rsa $BSDIR && chmod 777 $BSDIR/id_rsa
-docker build -t xxstop/wphp .
+docker build -t xxstop/wphp56 .
 rm $BSDIR/id_rsa
--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-CMD="docker run -d --name wphp --privileged
+CMD="docker run -d --name wphp56 --privileged
         -v /w:/w
         -p 80:80/tcp -p 80:80/udp
-        xxstop/wphp"
+        xxstop/wphp56"
 
 echo $CMD
 $CMD
