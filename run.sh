7cd6f296259e6b2f0deeaa30d11496a66a45304b
qiuwenxi <qiuwenxi@zbj.com>
2016-08-31 13:48:21 +0800

    Add php run

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name wphp56 --privileged
+        -v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
         -v /w:/w
         -p 80:80/tcp -p 80:80/udp
         xxstop/wphp56"
